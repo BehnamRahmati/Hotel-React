91e2fa4005df7ac56bd44bb219a5ba06e5831bb9
behnam <behnam.main@gmail.com>
2020-06-26 18:17:19 +0430

    set up router , pages , ready to build components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import React from 'react';
+import { Link, Route, Switch } from "react-router-dom"
+
+
 import './App.css';
+
+
+import Home from "./Pages/Home";
+import Error from "./Pages/Error";
+import Rooms from "./Pages/Rooms";
+import SingleRoom from "./Pages/SingleRoom";
 
 function App() {
   return (
-    <div className="App">
-      app
-    </div>
-  );
+    <>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/rooms/" component={Rooms} />
+        <Route exact path="/rooms/:room" component={SingleRoom} />
+        <Route component={Error} />
+      </Switch>
+    </>
 }
 
 export default App;
